5c99d59b7f6ec47059aea2361db59b85f822ba35
Mwangilwa Zimba <mwangilwa23@gmail.com>
2023-03-27 12:52:08 +0200

    Update main.c

--- main.c
+++ main.c
@@ -9,11 +9,10 @@
  */
 int main(void)
 {
-    int len;
-    int len2;
-    unsigned int ui;
+int len;
+int len2;
+unsigned int ui;
     void *addr;
-
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
     ui = (unsigned int)INT_MAX + 1024;
